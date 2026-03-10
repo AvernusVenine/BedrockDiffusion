630423efc91e1251ab10974253ce5cd963bd1fcd
sturm113 <tatesturm13@gmail.com>
2026-03-10 17:56:58 -0500

    Instead of working with a top and base, instead only considering the top as the base of formations should be exactly equal to the top of the next

--- Data.py
+++ Data.py
@@ -1,57 +1,77 @@
 import numpy as np
+import torch
 from pathlib import Path
 from sklearn.preprocessing import StandardScaler
+from torch.utils.data import Dataset
+from Noise import GaussianNoise
 
+class BedrockDataset(Dataset):
+    def __init__(self, data, context):
+        self.data = data
+        self.context = context
 
-def load_rasters(path):
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx], self.context[idx]
+
+def load_rasters(path, undiff_prefix='cmts'):
+    """
+    Loads all rasters stored as numpy arrays at a given path
+    :param path: Data path
+    :param undiff_prefix: Formation that borders very old undifferentiated bedrock
+    :return: List of formation elevation rasters as numpy arrays, Elevation raster as a numpy array
+    """
     path = Path(path).resolve()
 
-    top_files = [f for f in path.glob(f'**/*_top.npy') if f.is_file()]
-    base_files = [f for f in path.glob(f'**/*_base.npy') if f.is_file()]
+    files = [f for f in path.glob(f'**/*_top.npy') if f.is_file()]
 
-    top_rasters = [np.load(f) for f in top_files]
-    base_rasters = [np.load(f) for f in base_files]
+    rasters = [np.load(f) for f in files]
+    undiff = np.load(f'{path}/{undiff_prefix}_base.npy')
 
-    return top_rasters, base_rasters
+    rasters.append(undiff)
 
-def create_data(path, count=100):
-    """
-    Selects N random 200x200 pieces of land and compresses them into individual data pieces
-    :param path: Data path
-    :param count: Amount of pieces to generate
-    :return: List of data chunks
-    """
-    top_rasters, base_rasters = load_rasters(path)
     elevation = np.load(f'{path}/elevation.npy')
 
-    scaler = scaler_rasters(np.concatenate([top_rasters, base_rasters, [elevation]]))
+    return rasters, elevation
 
-    shape = top_rasters[0].shape
+def create_data(rasters, elevation, count=100, size=200):
+    """
+    Selects N random 200x200 pieces of land and compresses them into individual data pieces
+    :param rasters: Rasters numpy array
+    :param elevation: Elevation raster numpy array
+    :param count: Amount of data to generate
+    :param size: Resolution of data to generate
+    :return: Data tensor, Scaler
+    """
 
-    top_rasters = [scaler.transform(idx.reshape(-1, 1)).reshape(shape)[200:-1000, 200:-200] for idx in top_rasters]
-    base_rasters = [scaler.transform(idx.reshape(-1, 1)).reshape(shape)[200:-1000, 200:-200] for idx in base_rasters]
+    scaler = scaler_rasters(np.concatenate([rasters, [elevation]]))
+    shape = rasters[0].shape
 
+    rasters = [scaler.transform(idx.reshape(-1, 1)).reshape(shape)[200:-1000, 200:-200] for idx in rasters]
     elevation = scaler.transform(elevation.reshape(-1, 1)).reshape(shape)[200:-1000, 200:-200]
 
     data = []
 
     for _ in range(count):
-        x = np.random.randint(low=0, high=top_rasters[0].shape[0] - 500)
-        y = np.random.randint(low=0, high=top_rasters[0].shape[1] - 500)
+        x = np.random.randint(low=0, high=rasters[0].shape[0] - size)
+        y = np.random.randint(low=0, high=rasters[0].shape[1] - size)
 
-        arr = np.full((500, 500, 2*len(top_rasters) + 1), np.nan)
+        arr = np.full((size, size, len(rasters) + 1), np.nan)
 
-        for idx in range(len(top_rasters)):
+        for idx in range(len(rasters)):
 
-            arr[:, :, 2*idx] = top_rasters[idx][x:x+500, y:y+500]
-            arr[:, :, 2*idx + 1] = base_rasters[idx][x:x+500, y:y+500]
+            arr[:, :, idx] = rasters[idx][x:x+size, y:y+size]
 
         """Last channel of map should be elevation, possibly add geophysical data"""
-        arr[:, :, arr.shape[2] - 1] = elevation[x:x+500, y:y+500]
+        arr[:, :, arr.shape[2] - 1] = elevation[x:x+size, y:y+size]
 
         data.append(arr)
 
-    return data
+    data = torch.from_numpy(np.array(data, dtype=np.float32))
+
+    return data, scaler
 
 def scaler_rasters(rasters):
     scaler = StandardScaler()
--- DiffusionModel.py
+++ DiffusionModel.py
@@ -2,18 +2,75 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data import random_split, DataLoader
+from diffusers import UNet2DConditionModel, DDPMScheduler
+import Data
+from Data import BedrockDataset
 
-class ResidualBlock(nn.Module):
+def create_existence_mask(data):
 
-    def __init__(self):
-        super().__init__()
+    masks = []
+    for idx in range(int(data.shape[1]/2)):
+        top = torch.isnan(data[:, 2*idx, :, :])
+        base = torch.isnan(data[:, 2*idx + 1, :, :])
 
-    def forward(self, X):
+        existence = ~(top & base)
+        masks.append(existence.float().unsqueeze(1))
 
-        pass
+    return torch.cat(masks, dim=1)
 
-class DiffusionModel(nn.Module):
+def sanitise_input(data):
+    """
+    Replaces all nan elevation values with the highest elevation value in at a given location, effectively setting
+    the formation thickness to 0 meaning it does not exist
+    :param data: Data tensor
+    :return: Sanitised data tensor
+    """
+    elevation_max = np.nanmax(data, axis=-1, keepdims=True)
 
-    def __init__(self):
+    data = np.where(np.isnan(data), elevation_max, data)
+
+    return torch.tensor(data)
+
+def train_model(data_path, save_path, max_epochs=15, lr=1e-3):
+
+    rasters, elevation = Data.load_rasters(data_path)
+    data, scaler = Data.create_data(rasters, elevation, count=1000)
+
+    data = sanitise_input(data)
+
+    dataset = BedrockDataset(data[:, :, :, :len(rasters)], data[:, :, :, len(rasters):])
+
+    train_size = int(0.8 * len(dataset))
+    test_size = int(len(dataset) - train_size)
+    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    model = UNet2DConditionModel(
+        sample_size=200,
+        in_channels=2*len(top_rasters),
+        out_channels=3*len(top_rasters),
+    )
+
+    scheduler = DDPMScheduler(num_train_timesteps=1000, beta_schedule='squaredcos_cap_v2')
+
+    for epoch in range(max_epochs):
+        print(f'Epoch {epoch+1}')
+
+        model.train()
+
+        train_loss = 0
+        for data, context in train_loader:
+            data = data.to(device)
+            context = data.to(device)
+
+            noise = torch.randn(data.shape, device=device)
+            timestamps = torch.randint(
+                0,
+            )
 
         pass
